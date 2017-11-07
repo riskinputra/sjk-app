eb13ccc72fb09c5f65ab6f243160a32a5d5167ff
riskinputra <riski.n.putra@gmail.com>
2017-11-07 19:48:27 +0700

    vehicle-1

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser  = require('body-parser');
 
+
 const index   = require('./routers/index');
 const user    = require('./routers/user');
+const vehicle    = require('./routers/vehicle');
 
 const app = express();
 
@@ -10,12 +12,17 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/views'));
+// app.use('/assets', express.static('views'));
+
 app.set('views', './views') // specify the views directory
 app.set('view engine', 'ejs') // register the template engine
 
 // ------------ INDEX --------------
 app.use('/', index);
+// ------------- USER --------------
 app.use('/users', user);
+// ------------ VEHICLE -------------
+app.use('/vehicles', vehicle);
 
 app.listen(3000, function() {
   console.log('Sudah Nyambung ....!!!');
